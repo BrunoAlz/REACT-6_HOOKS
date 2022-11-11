8200bbafd6f1301163f2326639d21f8eee8a9099
Bruno Alves <bruno6821@gmail.com>
2022-11-11 17:57:09 -0300

    useReducer simples

--- react_hooks/src/components/HookUseReducer.js
+++ react_hooks/src/components/HookUseReducer.js
@@ -1,11 +1,16 @@
 import { useReducer } from "react";
 
 const HookUseReducer = () => {
+  const [number, dispatch] = useReducer((state, action) => {
+    return Math.random(state);
+  });
+
   return (
     <div>
       {/* 3 - useReducer */}
       <h2>useReducer</h2>
-
+      <p>Número: {number}</p>
+      <button onClick={dispatch}>Alterar Numero</button>
       <hr />
     </div>
   );
