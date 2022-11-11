ff3c96a8a1dde96545399ccd9148e932de38dffa
Bruno Alves <bruno6821@gmail.com>
2022-11-11 17:52:38 -0300

    useState e Inputs

---
+++ react_hooks/src/components/HookUseReducer.js
@@ -0,0 +1,14 @@
+import { useReducer } from "react";
+
+const HookUseReducer = () => {
+  return (
+    <div>
+      {/* 3 - useReducer */}
+      <h2>useReducer</h2>
+
+      <hr />
+    </div>
+  );
+};
+
+export default HookUseReducer;
--- react_hooks/src/components/HookUseState.js
+++ react_hooks/src/components/HookUseState.js
@@ -12,13 +12,35 @@ const HookUseState = () => {
     setName("Richard Alves");
   };
 
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(age);
+  };
+
   return (
     <div>
+      <hr />
       {/* 1 - useState */}
       <h2>useState</h2>
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar Nomes</button>
+      <hr />
+
+      {/* 2 - useState e inputs */}
+      <h2>useState e Inputs</h2>
+      <p>Digite a sua idade: </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Mudar idade" />
+      </form>
+      <p>Você têm {age} anos!</p>
       <hr />
     </div>
   );
